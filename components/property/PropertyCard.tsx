d807ab58c617d1c77234f50b53fde5421f04ca28
Kaiã Werb <30848004+kaiawerb@users.noreply.github.com>
2025-04-24 01:08:57 -0300

    PropertyCard Improves

--- components/property/PropertyCard.tsx
+++ components/property/PropertyCard.tsx
@@ -3,8 +3,10 @@ import {
   CardHeader,
   CardTitle,
   CardDescription,
+  CardContent,
 } from "@/components/ui/card"
-import Image from "next/image"
+
+import { Pencil, Eye, Trash2 } from "lucide-react"
 
 type PropertyCardProps = {
   imageUrl: string
@@ -15,13 +17,26 @@ type PropertyCardProps = {
 const PropertyCard = ({ imageUrl, name, city }: PropertyCardProps) => {
   return (
     <Card
-      className="border-none flex justify-start min-h-[200px]"
+      className="border-none flex min-h-[152px] w-full justify-between items-center bg-no-repeat bg-cover"
       style={{ backgroundImage: `url(${imageUrl})` }}
     >
       <CardHeader className="flex mt-auto space-y-0 p-5 text-white">
         <CardDescription className="text-xl text-white">{city}</CardDescription>
         <CardTitle className="text-2xl font-semibold">{name}</CardTitle>
       </CardHeader>
+      <CardContent className="mt-auto items-center">
+        <div className="flex gap-2">
+          <button className="bg-white rounded-full p-2">
+            <Eye size={20} strokeWidth={1.5} />
+          </button>
+          <button className="bg-yellow-300 rounded-full p-2 text-white">
+            <Pencil size={20} strokeWidth={1.5} />
+          </button>
+          <button className="bg-red-400 rounded-full p-2 text-white">
+            <Trash2 size={20} strokeWidth={1.5} />
+          </button>
+        </div>
+      </CardContent>
     </Card>
   )
 }
