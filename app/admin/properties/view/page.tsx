a174d2b19d5d52b20663bd46ef35c1d3ad9cfe65
Kaiã Werb <30848004+kaiawerb@users.noreply.github.com>
2025-04-27 02:46:51 -0300

    View PropertyPage Improves
    Layout Improves

--- app/admin/properties/view/page.tsx
+++ app/admin/properties/view/page.tsx
@@ -9,7 +9,7 @@ import PropertyCard from "@/components/property/PropertyCard"
 import PlaceHolderImage from "@/public/images/placeholdertemp.png"
 import dummyProperties from "@/dummyProperties.json"
 import { Badge } from "@/components/ui/badge"
-import { MapIcon, MapPin, Trash2 } from "lucide-react"
+import { MapIcon, MapPin, Phone, Trash2 } from "lucide-react"
 import { Avatar, AvatarFallback } from "@/components/ui/avatar"
 import Image from "next/image"
 import config from "@/lib/config"
@@ -98,28 +98,30 @@ const Page = async () => {
           </div>
         </div>
       </div>
-      <div className="w-2/4 overflow-hidden gap-3 flex flex-col">
+
+      <div className="w-2/4 overflow-hidden gap-6 flex flex-col">
         <div className="status flex flex-col justify-center text-center w-full h-[100px] bg-[#E52B44] rounded-xl">
           <h3 className="font-medium uppercase text-xl text-white">Vendido</h3>
           <span className="text-white uppercase">Contrato em 03/07/2023</span>
         </div>
 
-        <div className="owner-profile bg-light-100 flex flex-col justify-center p-8 rounded-md items-center">
-          <div className="justify-center flex mb-5">
-            <Image
-              src={`${config.env.imagekit.urlEndpoint}${session.user.image}`}
-              alt={`Profile Image ${session.user.fullname}`}
-              className="rounded-sm"
-              width={100}
-              height={100}
-            />
-          </div>
+        <div className="owner-profile bg-slate-50 flex flex-col justify-center p-8 rounded-md items-center">
+          <Image
+            src={`${config.env.imagekit.urlEndpoint}${session.user.image}`}
+            alt={`Profile Image ${session.user.fullname}`}
+            className="rounded-sm justify-center flex mb-5"
+            width={100}
+            height={100}
+          />
 
           <div className="flex flex-col max-md:hidden items-center">
             <p className="font-semibold text-xl text-[#444444]">
               {session?.user?.name}
             </p>
             <p className="text-[#525252]">Proprietário</p>
+            <button className="border border-red text-red justify-center text-center py-3 px-6 mt-5 rounded-lg flex gap-2">
+              <Phone strokeWidth={1.5} /> 51 9 8049-3798
+            </button>
           </div>
         </div>
       </div>
